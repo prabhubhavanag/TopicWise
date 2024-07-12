eea3f1007e3f89edcb92648683f666b8245871db
bprabhu <prabhubhavanag@gmail.com>
2024-07-12 16:37:26 +0530

    Prob1-committed

---
+++ problems/src/string/CountNoOfWords.java
@@ -0,0 +1,28 @@
+package string;
+
+import java.util.Arrays;
+
+public class CountNoOfWords {
+
+	public static void main(String[] args) {
+		int len = 0;
+		String givenString = "This is the sample string to be checked";
+
+		// string split way
+		len = givenString.split("\s").length;
+		System.out.println("string split way :: " + len);
+
+		// java 8 stream way
+		len = (int) Arrays.stream(givenString.split("\s")).count();
+		System.out.println("java 8 stream way :: " + len);
+
+		// char Array way
+		String trim = givenString.trim();
+		len = 0;
+		for (int i = 0; i < trim.length(); i++) {
+			if ((i == 0 && trim.charAt(i) != ' ') || (trim.charAt(i - 1) != ' ' && trim.charAt(i) == ' '))
+				len++;
+		}
+		System.out.println("char Array way :: " + len);
+	}
+}
